57b316969783e2ae4fb2e175448aa1382e9c1220
Le Vivilet <levivlet@pm.me>
2025-02-13 00:47:26 +0100

    feature: use import meta dirname

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import { dirname, join } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { join } from 'node:path'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = import.meta.dirname
 
 export const rgPath = join(
   __dirname,
