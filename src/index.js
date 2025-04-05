fd742c3bb6c138529e7a9449d4badcbb97c5e547
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-05 10:54:14 +0200

    feature: don't use import meta dirname, since it doesn't work well with jest (#30)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { join } from 'node:path'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const __dirname = import.meta.dirname
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export const rgPath = join(
   __dirname,
