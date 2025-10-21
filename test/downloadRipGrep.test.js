2ee74727bb5f8bdb9da2e2042b29894c742105ea
Le Vivilet <levivilet@pm.me>
2025-10-21 14:32:33 +0200

    feature: use native fetch (#72)

--- test/downloadRipGrep.test.js
+++ test/downloadRipGrep.test.js
@@ -136,6 +136,6 @@ test('downloadFile should handle download errors', async () => {
       join(tmpdir(), 'test.tar.gz'),
     ),
   ).rejects.toThrow(
-    `Failed to download \"https://github.com/microsoft/ripgrep-prebuilt/releases/download/v13.0.0-10/ripgrep-v13.0.0-10-x86_64-unknown-linux-musl.tar.gz\": RequestError: simulated error`,
+    `Failed to download \"https://github.com/microsoft/ripgrep-prebuilt/releases/download/v13.0.0-10/ripgrep-v13.0.0-10-x86_64-unknown-linux-musl.tar.gz\": simulated error`,
   )
 })
